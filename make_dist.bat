5553c5f7688ccf83f7e2c6f077cf36d26e362a44
unknown <jabel@JABEL-MOBL3.amr.corp.intel.com>
2014-04-11 09:34:35 -0700

    migrate installer to Python 34

--- make_dist.bat
+++ make_dist.bat
@@ -1,3 +1,2 @@
-REM cx_freeze doesnt seem to work yet with Python34 so use 33
-c:\Python33\python.exe setup.py sdist
-c:\Python33\python.exe setup.py bdist_msi
+c:\Python34\python.exe setup.py sdist
+c:\Python34\python.exe setup.py bdist_msi
