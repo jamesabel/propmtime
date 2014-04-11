5ba2d1f5f87c8565e48e5d276a7a744b888a582e
unknown <jabel@JABEL-MOBL3.amr.corp.intel.com>
2014-04-11 10:54:02 -0700

    cx_Freeze has a problem with Python 34 so stuck on 33

--- make_dist.bat
+++ make_dist.bat
@@ -1,2 +1,3 @@
-c:\Python34\python.exe setup.py sdist
-c:\Python34\python.exe setup.py bdist_msi
+REM cx_Freeze has a problem with Python 34 so I'm stuck on 33
+c:\Python33\python.exe setup.py sdist
+c:\Python33\python.exe setup.py bdist_msi
--- t.bat
+++ t.bat
@@ -1,1 +1,2 @@
+REM cx_Freeze has a problem with Python 34 so I'm stuck on 33
 c:\python33\scripts\nosetests.exe -s -v
