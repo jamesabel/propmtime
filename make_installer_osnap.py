9d731e4c30e1a711c8a6a20b2e855f771e3e1f8f
james abel <j@abel.co>
2017-08-17 10:46:37 -0700

    update web site

--- make_installer_osnap.py
+++ make_installer_osnap.py
@@ -5,9 +5,9 @@ import propmtime
 
 
 def make_installer():
-    osnap.installer.make_installer(propmtime.__python_version__, propmtime.__application_name__, propmtime.__author__,
-                                   'propagate the modification time up the directory/folder tree',
-                                   'https://github.com/jamesabel/propmtime')
+    osnap.installer.make_installer(propmtime.__python_version__, propmtime.__application_name__, propmtime.__version__,
+                                   propmtime.__author__, 'propagate the modification time up the directory/folder tree',
+                                   'www.abel.co')
 
 
 if __name__ == '__main__':
