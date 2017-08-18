46d90acf745c1fcd97aeeb75e91b7cdff1c1b1c2
james abel <j@abel.co>
2017-08-17 18:30:41 -0700

    windows support

---
+++ install_osnap.bat
@@ -0,0 +1,4 @@
+pushd .
+cd ..\osnap
+..\propmtime\venv\Scripts\python.exe setup.py install
+popd
--- make_venv.bat
+++ make_venv.bat
@@ -1,4 +1,4 @@
-\Users\james\AppData\Local\Programs\Python\Python36\python.exe -m venv --clear venv
+"C:\Program Files\Python36\python.exe" -m venv --clear venv
 venv\Scripts\pip3 install -U pip
 venv\Scripts\pip3 install -U setuptools
 venv\Scripts\pip3 install -r requirements.txt
