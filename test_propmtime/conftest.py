fc666e2943f690b00a1161698f84dcfa843c2f23
James Abel <j@abel.co>
2020-05-24 13:54:35 -0700

    improve log config

--- test_propmtime/conftest.py
+++ test_propmtime/conftest.py
@@ -12,6 +12,6 @@ log = get_logger(__file__)
 
 @pytest.fixture(scope="session", autouse=True)
 def tst_setup():
-    balsa = Balsa(__application_name__, __author__, log_directory="log", delete_existing_log_files=True, verbose=True)
+    balsa = Balsa(__application_name__, __author__, log_directory="log", delete_existing_log_files=True)
     balsa.init_logger()
     rmdir(Path(test_propmtime.data_parent))
