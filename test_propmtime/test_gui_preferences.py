d50e9d195e157cfc8e8784494e6d62bbe0ccfb38
James Abel <j@abel.co>
2022-06-08 12:04:56 -0700

    add semi-automated GUI test

---
+++ test_propmtime/test_gui_preferences.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from PyQt5.QtWidgets import QMessageBox
+
+from propmtime import __application_name__
+from propmtime.gui import PropMTimeSystemTray
+
+
+def test_gui_preferences(qtbot):
+
+    QMessageBox.information(None, __application_name__, "This is only semi-automated - tester human must click on the GUI windows as they appear")
+
+    log_file_path = Path("log", "test_gui_preferences.log")  # just for display - doesn't actually write to this file
+    propmtime_gui = PropMTimeSystemTray(qtbot, log_file_path)
+    propmtime_gui.preferences()
+    propmtime_gui.set_paths()
+    propmtime_gui.scan()
+    propmtime_gui.exit()
