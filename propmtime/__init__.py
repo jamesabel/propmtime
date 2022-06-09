aa029a71d5ae04d017b27a2c33255e3e85618fff
James Abel <j@abel.co>
2022-06-08 17:07:50 -0700

    process_hidden and process_system should have been default=False

--- propmtime/__init__.py
+++ propmtime/__init__.py
@@ -1,6 +1,6 @@
 # PEP 440 compliant
 # adhere to http://semver.org/
-__version__ = "0.8.6"
+__version__ = "0.8.7"
 
 __author__ = "abel"
 __application_name__ = "propmtime"
--- propmtime/gui/preferences.py
+++ propmtime/gui/preferences.py
@@ -6,8 +6,8 @@ from propmtime import __application_name__, __author__
 
 @attrs
 class PropMTimePreferences(Pref):
-    process_hidden: bool = attrib(default=None)
-    process_system: bool = attrib(default=None)
+    process_hidden: bool = attrib(default=False)
+    process_system: bool = attrib(default=False)
     process_dot_as_normal: bool = attrib(default=False)
     verbose: bool = attrib(default=False)
 
