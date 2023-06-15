3cf0ca743e4d8f6355e7c12f6abae27d9d1e3914
JamesAbel <j@abel.co>
2023-06-15 16:51:58 -0700

    move scripts to the scripts directory

---
+++ scripts/make_venv_dev.bat
@@ -0,0 +1,8 @@
+pushd .
+cd ..
+rmdir /S /Q venv
+"\Program Files\Python311\python.exe" -m venv --clear venv
+venv\Scripts\python.exe -m pip install --no-deps --upgrade pip
+venv\Scripts\pip.exe install -U setuptools
+venv\Scripts\pip.exe install -U -r requirements-dev.txt
+popd
